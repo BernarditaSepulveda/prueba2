506aa9640211a8348daa9f0dd44255aedd176e93
Bernardita Sepulveda <bernarditasepulveda@gmail.com>
2020-06-11 20:50:30 -0400

    metodo url 1

--- nasa.rb
+++ nasa.rb
@@ -17,26 +17,3 @@ end
 data = get_data(url)
 
 print data
-
-# File.write('./index.html', 
-
-# n_externo = ARGV[0].to_i
-# n_interno = ARGV[1].to_i
-
-# puts "<html>"
-# puts "<head>"
-# puts "</head>"
-# puts "<body>"
-# puts "<ul>"
-# n_externo.times do |j|
-#     puts "<li>\n"
-#     puts "\t<ul>"
-#     n_interno.times do |i|
-#         puts "<li> #{j}.#{i} </li>"
-#     end
-#     puts "\t</ul>"
-#     puts "</li>"
-# end
-# puts "</ul>"
-# puts "</body>"
-# puts "</html>")
